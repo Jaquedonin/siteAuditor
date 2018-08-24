65686d8eb2012f557976a64f9ae65e78f81bbfc1
mariannactx <contato@mariannactx.com>
2018-08-24 15:44:54 -0300

    Logo TCE. Iniciar estrutura base do painel de gerenciamento.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lavid' });
+    res.render('index', { 
+        title: 'Lavid',
+        escola: {
+            nome: 'E.E.P Euniz Argemiro',
+            cidade: 'João Pessoa'
+        },
+        user:  {
+            nome: 'Maria José Gonçalves',
+            foto: 'https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2300002206678298&height=50&width=50&ext=1537571297&hash=AeTfvLLcNA-8aRmh'
+        }
+    });
 });
 
 module.exports = router;
