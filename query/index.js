afd3ae80c0e660cf45dc5d5ff78483a842ae0987
Jaque <jaquelinedonin@gmail.com>
2018-09-11 16:01:06 -0300

    SQL de vídeos.

---
+++ query/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	updateOne : function(table, id, url){
+		return "UPDATE "+ table +" SET url = '"+url+"' WHERE id = " + id
+	},
+	deleteOne : function(table, id){
+		return "DELETE FROM "+ table +" WHERE id = " + id
+	}, 
+	findAll : function(table, id){
+		return "SELECT * FROM "+ table +" WHERE professor_id = " + id
+	}
+}
+
