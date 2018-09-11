e94ea359bf414d0915e3ba9c1fde5ed4c0facbd0
Jaque <jaquelinedonin@gmail.com>
2018-09-11 15:57:50 -0300

    API de CRUDE de vídeos

---
+++ routes/videos.js
@@ -0,0 +1,65 @@
+var database = require('../database/database');
+var query = require('../query');
+var express = require('express');
+var router = express.Router();
+
+
+router.put('/videos', function(req, res, next) {
+	if(req.body.id === undefined)
+		return false;
+	else{
+		database.connection.query(query.updateOne("videos", req.body.id, req.body.url), function (err, result) {
+			if(err){
+				res.setHeader('Content-Type', 'application/json');
+				return res.status(400).send(err);
+			}
+			else{
+				res.setHeader('Content-Type', 'application/json');
+				res.json(result);
+				res.end('video')
+			}
+		});
+	}
+});
+
+router.delete('/videos', function(req, res, next) {
+	if(req.body.id === undefined)
+		return false;
+	else{
+		database.connection.query(query.deleteOne("videos", req.body.id), function (err, result) {
+			if(err){
+				res.setHeader('Content-Type', 'application/json');
+				return res.status(400).send(err);
+			}
+			else{
+				res.setHeader('Content-Type', 'application/json');
+				res.json(result);
+				res.end('video')
+			}
+		});
+	}
+});
+
+router.get('/videos/:id', function(req, res, next) {	
+
+	if(req.params.id === undefined){
+		return res.status(400).send(err);
+	}
+	else{
+		database.connection.query(query.findAll("videos", req.params.id), function (err, result) {
+			if(err){
+				res.setHeader('Content-Type', 'application/json');
+				return res.status(400).send(err);
+			}
+			else{
+				res.setHeader('Content-Type', 'application/json');
+				res.json(result);
+				res.end('video')
+			}
+		}); 	
+	}
+});
+
+
+
+module.exports = router;
