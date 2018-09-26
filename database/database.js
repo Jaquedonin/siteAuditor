074486852fe1d6aaeb5f0295bfc8f28b002af168
Jaque <jaquelinedonin@gmail.com>
2018-09-26 17:39:07 -0300

    03
    create tables for database tce_mapa

--- database/database.js
+++ database/database.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     port               : 3306,
     user               : 'root',
     password           : '',
-    database           : 'tce-mapa'//,
+    database           : 'tce_mapa'//,
     //multipleStatements : true
 });
 
