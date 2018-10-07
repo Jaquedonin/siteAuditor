7255adfa50b73607788b7f2b6b11cd3d55dc3541
Jaque <jaquelinedonin@gmail.com>
2018-10-06 21:19:51 -0300

    identações

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ var express = require('express');
 var router = express.Router();
 var database = require('../database/database');
 var query = require('../query');
-
 var fs = require('fs');
 var data = JSON.parse(fs.readFileSync('./database/data.json', 'utf8'));
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,7 +18,8 @@ router.get('/', function(req, res, next) {
 
 router.get('/bem-vindo', function(req, res, next) {
     
-    if(!req.session.user){
+    if(!req.session.user)
+    {
         //data.user = false;
         data.professores = false;
         return res.redirect("/auth");
@@ -27,12 +30,14 @@ router.get('/bem-vindo', function(req, res, next) {
 });
 
 router.get('/galeria', function(req, res, next) {
+    
     var options = { 
         method: 'GET'
        
     };
 
     request(options, function (error, response, body) {
+        
         if (error) throw new Error(error);
 
         res.render('gallery', response);
@@ -43,7 +48,8 @@ router.get('/galeria', function(req, res, next) {
 
 router.post('/register', function(req, res, next){
 
-    var userData = {
+    var userData = 
+    {
         fbUser: req.body["fb-register-user"], 
         email: req.body.email,
         senha: req.body.senha
@@ -51,25 +57,34 @@ router.post('/register', function(req, res, next){
 
     database.connection.connect(function(err){
         
-        if(err) {
+        if(err) 
+        {
 
             res.status(500).json({
                 error: 1,
                 data: "Internal Server Error"
             });
 
-        } else {
+        } 
+
+        else 
+        {
             database.connection.query(
                 'INSERT INTO professores SET ?', 
                 [userData], 
-                function(err, rows, fields) {
+                function(err, rows, fields) 
+                {
                 
-                    if (err) {
+                    if (err) 
+                    {
                         res.status(400).json({
                             error: 1,
                             data: "Error Occured!"
                         });
-                    } else {             
+                    } 
+
+                    else 
+                    {             
                         res.status(201).json({
                             error: 0,
                             data: "User registered successfully!" 
@@ -115,42 +130,51 @@ router.get('/dashboard', function(req, res, next) {
         }
     }); 
     */
-    if(!req.session.token){
+    if(!req.session.token)
+    {
         return res.redirect("/auth");
     }
 
     data.user = true;
     console.log("testes")
-    database.connection.query(query.findAllVideos(req.session.professorId), function (err, result) {
-        if(!err){
+    database.connection.query(query.findAllVideos(req.session.professorId), function (err, result) 
+    {
+        if(!err)
+        {
             console.log(result)
             data.videos = result;
             console.log(result)
             database.connection.query(query.findAll('cidades'), function (err, result) {
-                if(!err){
-
+                
+                if(!err)
+                {
                     data.cidades = result;
                     console.log(result)
                     res.render('dashboard', data);
                 }
-            });  
-        }else{
+            }); 
+        }
+
+        else
+        {
             console.log(err);
         }
     });
-     
+
 });
 
 router.get('/auth', function(req, res, next) {
+    
     res.render('auth');
 });
 
-router.post('/galeria', function(req, res){
-    
+router.post('/galeria', function(req, res)
+{
     var codigo = req.body.cidade;
     var cidades = JSON.parse(fs.readFileSync('public/javascripts/cidades.json', 'utf8'));
 
     res.status(200).json({
+        
         error: 0,
         data: cidades[codigo]
     });
