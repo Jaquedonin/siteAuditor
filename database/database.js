e90ee1a1521b562969f2fd2c69307f70d5799a06
Marianna Cruz <mariannactx@gmail.com>
2018-10-22 09:19:54 -0300

    Dados do banco de dados para o heroku

--- database/database.js
+++ database/database.js
@@ -2,11 +2,11 @@ var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
     //connectionLimit    : 100,
-    host               : 'localhost',
+    host               : 'us-cdbr-iron-east-01.cleardb.net',
     port               : 3306,
-    user               : 'root',
-    password           : '',
-    database           : 'tce-mapa'//,
+    user               : 'be7f7ab045f386',
+    password           : 'ff84709c',
+    database           : 'heroku_b06458221811ff6',
     //multipleStatements : true
 });
 
