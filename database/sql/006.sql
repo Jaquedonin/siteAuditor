bc53fb2c95c02074156ecdd0882641ecb0769370
Marianna Cruz <mariannactx@gmail.com>
2018-10-27 18:38:30 -0300

    Adicionar campos professor e escola.

--- database/sql/006.sql
+++ database/sql/006.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `tce-mapa`.`videos` 
 ADD COLUMN `views` INT(11) NOT NULL AFTER `categoria_id`;
+CHANGE COLUMN `url` `url` VARCHAR(255) NOT NULL AFTER `categoria_id`,
+CHANGE COLUMN `professor_escola_id` `professor_id` INT(11) NOT NULL ,
+CHANGE COLUMN `cidade` `cidade_id` VARCHAR(255) NULL DEFAULT NULL ,
+ADD COLUMN `escola_id` VARCHAR(45) NULL AFTER `professor_id`;
