05915ca241a7cdd0928e32599614006f5d190971
Marianna Cruz <mariannactx@gmail.com>
2018-10-27 16:19:32 -0300

    Adicionar campo de views à tabela de vídeos

--- database/sql/006.sql
+++ database/sql/006.sql
@@ -2,4 +2,5 @@ ALTER TABLE `tce-mapa`.`videos`
 ADD COLUMN `thumb` VARCHAR(255) NOT NULL AFTER `url`;
 ADD COLUMN `descricao` LONGTEXT NOT NULL AFTER `thumb`;
 ADD COLUMN `autor` VARCHAR(45) NOT NULL AFTER `categoria_id`;
+ADD COLUMN `views` INT(11) NOT NULL AFTER `descricao`;
 
