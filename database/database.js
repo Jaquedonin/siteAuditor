f8c4857e8c07c05592dae20cd5f54c9d09d192a8
Marianna Cruz <mariannactx@gmail.com>
2018-11-06 09:44:29 -0300

    Utilizar pooling na conexão ao banco de dados

--- database/database.js
+++ database/database.js
@@ -1,13 +1,7 @@
 var mysql      = require('mysql');
 
-var connection = mysql.createConnection({
-    //connectionLimit    : 100,
-    host               : 'us-cdbr-iron-east-01.cleardb.net',
-    port               : 3306,
-    user               : 'be7f7ab045f386',
-    password           : 'ff84709c',
-    database           : 'heroku_b06458221811ff6',
-    //multipleStatements : true
+var pool = mysql.createPool({
+    connectionLimit    : 10,
 });
 
-module.exports.connection = connection;
+module.exports.pool = pool;
