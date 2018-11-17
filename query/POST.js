6884b232a9b682d80b46fb79638a64d6fdf215c0
Jaque <jaquelinedonin@gmail.com>
2018-11-17 19:23:40 -0300

    Atualização do login

--- query/POST.js
+++ query/POST.js
@@ -1,23 +1,60 @@
 
 var requesting = function(email,senha){
 	var request = require('request');
-	request.post({
-	    "headers": { "content-type": "application/json" },
-	    "url": "http://obras4d.tce.pb.gov.br:33307/api/auth/signin",
-	    "body": JSON.stringify({
-			  emailOrCpf : email,
-			  password : senha
-	})
-	}, (error, response, body) => {
-	    if(!error) {
-	        return false;
-	    }
-	    //console.dir(body);
-	    return true;
+	return new Promise(function(resolve, reject)
+	{
+		request.post({
+		    "headers": { "content-type": "application/json" },
+		    "url": "http://150.165.204.115:8081/api/map/users",
+		    "body": JSON.stringify({
+				  emailOrCpf : email,
+				  password : senha
+		})
+		}, (error, response, body) => 
+		{
+			var response = JSON.parse(body);
+			//console.log(response.status );
+		    if (response.status == 401) 
+            {
+               // var result = [false];
+				 //console.log(body);
+				var result = [false];
+                resolve(false);
 
-	    
+            }
+            
+            
+            var response = JSON.parse(body);
+		    var result = [response.name, response.email ];
+		   // {result: {email: response.name}};
+		    //console.dir(result);
+		    resolve({name: response.name, email: response.email});
+		    		    
+		    //console.dir(response);
+		    //console.dir(body);
+		    //return true;
+            
+            
+
+		    
+		});
 	});
 }
 
-//requisicao("tceuser@tce.com", "l4v1d123");
+/*requesting("tceuser@tce.com", "l4v1d123").then(function(result){
+	//console.log("resolve");
+		if(result != false){
+			console.log("reason");
+			console.log(result.name);
+			//console.log(result[1]);	
+		}
+		//console.log(resolve[0]);
+	
+
+});
+	
+*/
+
+
 module.exports.requesting = requesting;
+
--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ var jwt = require('jsonwebtoken');
 var requisicao = require("../query/POST");
 var model = require("../models/professores");
 
+
 auth.use(cors());
 
 process.env.SECRET_KEY = "devesh";
@@ -26,78 +27,48 @@ auth.get('/logout', function(req, res) {
 });
 
 auth.post('/login', function(req, res) {
-
-    var email = req.body.email;
-    var senha = req.body.senha;
-    var request = require('request');
-    request.post({
-        "headers": { "content-type": "application/json" },
-        "url": "http://obras4d.tce.pb.gov.br:33307/api/auth/signin",
-        "body": JSON.stringify({
-              emailOrCpf : email,
-              password : senha
-    })
-    }, (error, response, body) => 
-    {
-        if(error) 
-        {
-            console.log("Não Encontrou no banco do tce");
-            return res.redirect('/');
-        }
-        //console.dir(body);
-        console.log("Encontrou no banco do tce");
-        model.find({where: {email: req.body.email}}).then(function(result)
-        {
-            
-            if(!result.length)
-            {
-                model.insertOne(req).then(function (result) {
-                    console.log("Deu certo a inserçao");
-                    console.log(result);
-                    return res.redirect('/auth'); 
-                });
-            }
-
-            if(result[0].senha != req.body.senha)
-            {
-                return res.redirect('/');
-                // res.status(204).json({
-                //     error: 1,
-                //     data: 'Email e senha incorretos'
-                // });
-            }
-        
-            var payload = JSON.parse(JSON.stringify(result[0]));  
-            req.session.token = jwt.sign(
-                payload, process.env.SECRET_KEY, { expiresIn: 1440 }
-            );
-            req.session.professorId = result[0].id;
-            req.session.user = true;
-
-            return res.redirect("/bem-vindo");
     
-        });       
-    });
+    var email = req.body.email;     
+    var senha = req.body.senha;          
+    model.findTCE(email, senha).then(function(tce)
+    {         console.log(tce);
+        // tce = {email: email, senha: senha, nome: name};          
+        if(tce )
+        {              
+            var email = tce.email;             
+            var nome = tce.name;
+            console.log(email);
+            console.log(nome); 
+
+            model.find(email).then(function(result)
+            {                 
+                // result = {id: 1, nome: "Maria"} || false                  
+                    if(!result)
+                    {                     
+                        var result = model.insertOne(email, nome).then(function (insert)
+                        {
+                            console.log("Insert ",insert);
+                            return [{id:insert.insertId}]; 
+                        });                 
+                    }
+                console.log("result ",result);                  
+                var payload = JSON.parse(JSON.stringify(result[0]));                 
+                req.session.token = jwt.sign(                     
+                payload, process.env.SECRET_KEY, { expiresIn: 1440 });                  
+                req.session.professorId = result[0].id;                 
+                req.session.professorNome = nome;                 
+                req.session.user = true;                 
+                res.redirect("/bem-vindo");             
+            });
+
+        } 
+        else{
+            res.redirect("/auth"); 
+        }                 
+            
+    }); 
+    
 });
 
-var requesting = function(email,senha){
-    var request = require('request');
-    request.post({
-        "headers": { "content-type": "application/json" },
-        "url": "http://obras4d.tce.pb.gov.br:33307/api/auth/signin",
-        "body": JSON.stringify({
-              emailOrCpf : email,
-              password : senha
-    })
-    }, (error, response, body) => {
-        if(error) {
-            return false;
-        }
-        //console.dir(body);
-        return true;
-
-        
-    });
-}
 
 module.exports = auth;
