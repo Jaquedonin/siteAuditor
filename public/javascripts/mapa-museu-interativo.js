30ee63cf4d4b14269abcd8825e49cd355e0369a8
Marianna Cruz <mariannactx@gmail.com>
2018-12-07 08:02:55 -0300

    auto stop para youtube

--- public/javascripts/mapa-museu-interativo.js
+++ public/javascripts/mapa-museu-interativo.js
@@ -1,4 +1,5 @@
 var modal;
+var player;
 
 window.addEventListener("load", function(){
     modal = $("#modal-play-video");
@@ -13,12 +14,44 @@ window.addEventListener("load", function(){
         modal.modal("hide");
         
         if(e.key == "video"){
+            
             $("#iframe-play-video").attr("src", e.newValue);
+            
+            var regExprYt = /https:\/\/youtube\.com\/embed\/(.*)(\?.*)/;
+            var regExprFb = "http(?:s?):\/\/(?:www\.)?facebook\.com\/(?:.*\/)(?:videos\/)(.*)(?:\/)";
+    
+            var matchYt = e.newValue.match(regExprYt);
+            var matchFb = e.newValue.match(regExprFb);
+
+            console.log(matchYt);
+
+            if(matchYt){
+                player = new YT.Player('iframe-play-video', {
+                    height: '390',
+                    width: '640',
+                    videoId: matchYt[2],
+                    events: {
+                        'onStateChange': ytStateChange
+                    }
+                });
+            }
+
             modal.modal("show");
         }
         
     });
 })
+
+function ytStateChange(event) {
+    console.log(event);
+    if (event.data == YT.PlayerState.PLAYING) {
+        setTimeout(stopVideo, 6000);
+    }
+}
+
+function stopVideo(){
+    // modal.modal("hide");
+}
 
 function visualizarVideo(id){
 
@@ -28,7 +61,7 @@ function visualizarVideo(id){
 
         $("#trigger-play-video").on("click", function(e){
             var videoUrl = $(e.target).attr("data-video");
-            localStorage.setItem("video", videoUrl + "?rel=0&autoplay=1");
+            localStorage.setItem("video", videoUrl + "?enablejsapi=1");
         });
         
         $("#visualizar-video").modal("show");
