ffd2bb5b7bd63f14f6fb10e30b1cc2cc61cdb6d1
Marianna Cruz <mariannactx@gmail.com>
2018-12-10 10:28:09 -0300

    corrigir carregamento apos primeiro video do youtube

--- public/javascripts/mapa-museu-interativo.js
+++ public/javascripts/mapa-museu-interativo.js
@@ -13,40 +13,41 @@ window.addEventListener("load", function(){
             
             onStopYtVideo();
             onStopFbVideo();
-
-            var video = JSON.parse(e.newValue);
-
-            var isYt = video.type == "yt";
-            var isFb = video.type == "fb";
             
-            if(isYt){
+            setTimeout(function(){
 
+                var video = JSON.parse(e.newValue);
 
-                ytPlayer = new YT.Player('yt-player', {  
-                    width: "100%",
-                    height: "100%",
-                    videoId: video.id,
-                    events: {
-                        'onReady': onReadyYtVideo,
-                        'onStateChange': onStopYtVideo
-                    }
-                });
-            }
-            
-            if(isFb){             
+                var isYt = video.type == "yt";
+                var isFb = video.type == "fb";
+                
+                if(isYt){
+                    ytPlayer = new YT.Player('yt-player', {  
+                        width: "100%",
+                        height: "100%",
+                        videoId: video.id,
+                        events: {
+                            'onReady': onReadyYtVideo,
+                            'onStateChange': onStopYtVideo
+                        }
+                    });
+                }
+                
+                if(isFb){             
 
-                $("#fb-player").html("<div id='fb-player-video' class='fb-video'></div>");
+                    $("#fb-player").html("<div id='fb-player-video' class='fb-video'></div>");
 
-                $("#fb-player-video").attr("data-href", "https://www.facebook.com/video/video.php?v="+ video.id);
-                $("#fb-player-video").attr("data-show-text", "false");
-                $("#fb-player-video").attr("data-allowfullscreen", "true");
-                $("#fb-player-video").attr("data-autoplay", "true");
+                    $("#fb-player-video").attr("data-href", "https://www.facebook.com/video/video.php?v="+ video.id);
+                    $("#fb-player-video").attr("data-show-text", "false");
+                    $("#fb-player-video").attr("data-allowfullscreen", "true");
+                    $("#fb-player-video").attr("data-autoplay", "true");
 
-                $("#fb-player-video").css("width", window.innerHeight + "px");
-                $("#fb-player-video").css("height", window.innerHeight + "px");
+                    $("#fb-player-video").css("width", window.innerHeight + "px");
+                    $("#fb-player-video").css("height", window.innerHeight + "px");
 
-                FB.XFBML.parse();
-            }
+                    FB.XFBML.parse();
+                }
+            }, 2000);
             
         }
         
@@ -65,6 +66,7 @@ function onStopYtVideo(event){
         setTimeout(function(){
             ytPlayer.destroy();
             ytPlayer = null;
+            $("body").append("<div id='yt-player'></div>")
         }, 1000);
     }
 }
